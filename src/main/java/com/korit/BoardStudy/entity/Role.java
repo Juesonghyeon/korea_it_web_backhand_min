c2e671b23611c32031790399f09c2be37d5ec810
Juesonghyeon <199971alfks@gmail.com>
2025-07-02 21:40:17 +0900

    first commit

---
+++ src/main/java/com/korit/BoardStudy/entity/Role.java
@@ -0,0 +1,10 @@
+package com.korit.BoardStudy.entity;
+
+public class Role {
+    private Integer roleId;
+
+    private Integer RoleId;
+    private String RoleName;
+    private String RoleNameKor;
+
+}
