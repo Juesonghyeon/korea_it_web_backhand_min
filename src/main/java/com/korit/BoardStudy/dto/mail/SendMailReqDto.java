9fcbf9f196a59b6c375a4b8faadad88cd4867e66
Juesonghyeon <199971alfks@gmail.com>
2025-07-08 19:09:17 +0900

    Feat:commit

---
+++ src/main/java/com/korit/BoardStudy/dto/mail/SendMailReqDto.java
@@ -0,0 +1,11 @@
+package com.korit.BoardStudy.dto.mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SendMailReqDto {
+    private String email;
+
+}
